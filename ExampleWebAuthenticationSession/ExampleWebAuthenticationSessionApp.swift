1c2daf963bf1f8853982e830fad1400645a355a1
mtgto <hogerappa@gmail.com>
2025-04-15 19:47:50 +0900

    Initial Commit

---
+++ ExampleWebAuthenticationSession/ExampleWebAuthenticationSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleWebAuthenticationSessionApp.swift
+//  ExampleWebAuthenticationSession
+//
+//  Created by mtgto on 2025/04/15.
+//
+
+import SwiftUI
+
+@main
+struct ExampleWebAuthenticationSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
